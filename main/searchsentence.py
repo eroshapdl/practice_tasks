86b00cea2c37368828cca0a79bf30ec35be50761
eroshapdl <58264020+eroshapdl@users.noreply.github.com>
2023-05-25 17:00:32 +0545

    added searchsentence

---
+++ main/searchsentence.py
@@ -0,0 +1,23 @@
+def search_sentences(sentences, search):
+    results = []
+    for sentence in sentences:
+        if search in sentence:
+            results.append(sentence)
+            break
+    return results
+
+sentences = [
+    "iam eru",
+    "eru paudel",
+    "hellooooooooooo",
+    "sup eru",
+    "ttyl ok bye",
+    "aajha jado cha",
+]
+
+search = "eru"
+results = search_sentences(sentences, search)
+
+print(search)
+for sentence in results:
+    print(sentence)

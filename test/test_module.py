50b8d6c03b5dcad7d86f6af08ea3e5fa092440bb
eroshapdl <58264020+eroshapdl@users.noreply.github.com>
2023-05-25 11:40:40 +0545

    minor changes

--- test/test_module.py
+++ test/test_module.py
@@ -14,4 +14,4 @@ def test_count_characters():
 
     assert count_characters("a") == {"a": 1}
 
-    assert count_characters("hellooo") == {"h": 1, "e": 1, "l": 2, "o": 1}
+    assert count_characters("helloooo") == {"h": 1, "e": 1, "l": 2, "o": 1}
